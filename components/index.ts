55f5848b295e691ba6975baf9417026be8cd8d13
georgefrg <georgeftw00@gmail.com>
2024-08-06 01:04:33 +0300

    finished

---
+++ components/index.ts
@@ -0,0 +1,22 @@
+import Footer from "./Footer";
+import Hero from "./Hero";
+import NavBar from "./NavBar";
+import CustomButton from "./CustomButton";
+import SearchBar from "./SearchBar";
+import CustomFilters from "./CustomFilters";
+import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+import CarDetails from "./CarDetails";
+import ShowMore from "./ShowMore";
+export {
+  Hero,
+  ShowMore,
+  CustomButton,
+  SearchManufacturer,
+  CustomFilters,
+  SearchBar,
+  Footer,
+  NavBar,
+  CarCard,
+  CarDetails,
+};
